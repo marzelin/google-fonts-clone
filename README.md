3ba709537091cbf7fa48dcab7daff7b3c5f6dc88
marzelin <marzelin@gmail.com>
2017-10-04 12:03:17 +0200

    modify project description and add readme

---
+++ README.md
@@ -0,0 +1,12 @@
+
+<h1>Google Fonts Clone</h1>
+<p>
+  This project shows how efficient react + redux can be and how easy it is
+  to create complex user interfaces in these technologies. It renders
+  hundreds of font cards that can be filtered with advanced search options
+  in realtime. Go back to home page, hit search icon and see it in action.
+</p>
+<p>
+  In addition you can see responsive behavior in adjusting user interface
+  to device capabilities.
+</p>
--- src/components/about.js
+++ src/components/about.js
@@ -6,17 +6,18 @@ export default () => {
     <div className={styles.default}>
       <h1>Google Fonts Clone</h1>
       <p>
-        This project shows how efficient react + redux can be. It renders
+        This project shows how efficient react + redux can be and how easy it is
+        to create complex user interfaces in these technologies. It renders
         hundreds of font cards that can be filtered with advanced search options
-        in realtime with no jank whatsoever. Hit search icon and see it in
-        action.
+        in realtime. Go back to home page, hit search icon and see it in action.
       </p>
       <p>
         In addition you can see responsive behavior in adjusting user interface
         to device capabilities.
       </p>
       <p>
-        source code is <a>here</a>
+        project source code is{" "}
+        <a href="https://github.com/marzelin/google-fonts-clone">here</a>
       </p>
     </div>
   );
